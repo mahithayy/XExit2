b8d6175bc4b875b2082f46f37be144af2af4304d
mahithayy <hollamahitha@gmail.com>
2025-07-25 11:31:25 +0530

    import function

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { submitResignation, submitExitResponses } = require('../controllers/userController');
+const { submitResignation,  submitQuestionnaire} = require('../controllers/userController');
 const { authenticateEmployee } = require('../middleware/auth');
 
 // Employee submits resignation
 router.post('/resign', authenticateEmployee, submitResignation);
 
 // Employee submits exit interview
-router.post('/responses', authenticateEmployee, submitExitResponses);
+router.post('/responses', authenticateEmployee, submitQuestionnaire);
 
 module.exports = router;
